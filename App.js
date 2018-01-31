dfb07e363b54654f7a5b2689a2efc73524c19308
Thomas_Johnson <tjohnson1106@gmail.com>
2018-01-31 15:38:50 -0500

    Viewport.js iOS tab bar example created

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+import Viewport from "./src/Viewport";
+
+export default class App extends React.Component {
+  render() {
+    return <Viewport />;
+  }
+}
---
+++ src/Viewport.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Image, Text, TabBarIOS } from "react-native";
+
+const homeIcon = require("./images/home.png");
+const favIcon = require("./images/star.png");
+const blogIcon = require("./images/notebook.png");
+const profileIcon = require("./images/user.png");
+
+class Viewport extends Component {
+  state = {
+    selected: "home"
+  };
+
+  selectTab(id) {}
+
+  renderTab(options) {
+    return (
+      <TabBarIOS.Item
+        title={options.title}
+        selected={this.state.selected === options.id}
+        onPress={() => this.selectTab(options.id)}
+        icon={options.icon}
+      >
+        <View style={StyleSheet.container}>
+          <Image source={options.icon} styles={styles.icon} />
+          <Text style={StyleSheet.title}>{options.title}</Text>
+        </View>
+      </TabBarIOS.Item>
+    );
+  }
+
+  render() {
+    return (
+      <TabBarIOS tintColor="#42b49a">
+        {this.renderTab({
+          title: "Home",
+          id: "home",
+          icon: homeIcon
+        })};
+        {this.renderTab({
+          title: "Favorites",
+          id: "favorites",
+          icon: favIcon
+        })};
+        {this.renderTab({
+          title: "Blog",
+          id: "blog",
+          icon: blogIcon
+        })};
+        {this.renderTab({
+          title: "Profile",
+          id: "profile",
+          icon: profileIcon
+        })};
+      </TabBarIOS>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justiftyContent: "center"
+  },
+  titlle: {
+    fontSize: 20,
+    marginTop: 20
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    tintColor: "#42b49a"
+  }
+});
+
+export default Viewport;
